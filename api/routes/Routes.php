4043cd9f80ede7e163ee4e6ac9fb00a78d8775b5
ramiro <mostlyphpsoftware@gmail.com>
2024-01-25 18:30:47 -0700

    ruta principal habilitada

--- api/routes/Routes.php
+++ api/routes/Routes.php
@@ -8,11 +8,42 @@
             if (count($routesArray) == 0) {
                 header("Location: template.php");
             }
-            echo json_encode ([
-                "status" => 200,
-                "result" => "success",
-                "uri" => $routesArray
-            ]);
+            else if (count($routesArray) == 1 && isset($_SERVER['REQUEST_METHOD'])) {
+                if ($_SERVER['REQUEST_METHOD'] == "GET") {
+                    echo json_encode ([
+                        "status" => 200,
+                        "result" => "GET",
+                        "uri" => $routesArray
+                    ]);
+                } else if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                    echo json_encode ([
+                        "status" => 200,
+                        "result" => "POST",
+                        "uri" => $routesArray
+                    ]);
+                } else if ($_SERVER['REQUEST_METHOD'] == "PUT") {
+                    echo json_encode ([
+                        "status" => 200,
+                        "result" => "PUT",
+                        "uri" => $routesArray
+                    ]);
+                } else if ($_SERVER['REQUEST_METHOD'] == "DELETE") {
+                    echo json_encode ([
+                        "status" => 200,
+                        "result" => "DELETE",
+                        "uri" => $routesArray
+                    ]);
+                } else {
+                    header("Location: template.php");
+                }
+            }
+            else {
+                echo json_encode ([
+                    "status" => 200,
+                    "result" => "success",
+                    "uri" => $routesArray
+                ]);
+            }
         }
 
     }
