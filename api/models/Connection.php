c5c258ae484726f1d81258d090dd0b4ba71e59f1
ramiro <mostlyphpsoftware@gmail.com>
2024-01-25 21:47:30 -0700

    adding the pdo connection

--- api/models/Connection.php
+++ api/models/Connection.php
@@ -1,5 +1,27 @@
 <?php
 
-    
+    class Connection {
+
+        public static function info () {
+            return [
+                "database" => "database-1",
+                "user" => "adaptowebs",
+                "pass" => "adaptowebs.Rg2"
+            ];
+        }
+
+        public static function connect () {
+            try {
+                $conn = new PDO("mysql:host=localhost;dbname=".Connection::info()['database'], 
+                Connection::info()['user'], Connection::info()['pass']);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $conn->exec("set names utf8");
+                return $conn;
+            } catch (PDOException $e) {
+                die("Error ".$e->getMessage());
+            }
+        }
+
+    }
 
 ?>
